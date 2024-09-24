6ef6b11e9703e3588556502c524b8eb44aa1c2ce
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-09-23 22:08:06 -0300

    feat(App): add method to check if two lists have the same size

--- src/main/java/ifsp/tc1/App.java
+++ src/main/java/ifsp/tc1/App.java
@@ -1,7 +1,21 @@
 package ifsp.tc1;
 
 public class App {
-    public static int[] intercalate(int[] a, int[] b) {
+    private int n;
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public boolean listsHaveNLength(int[] a, int[] b) {
+        return a.length == n && b.length == n;
+    }
+
+    public int[] intercalate(int[] a, int[] b) {
         int[] c = {};
         return c;
     }
