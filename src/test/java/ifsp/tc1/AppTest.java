dd5099d5b32515b59a86a020ea71074d00b46fca
Vinicius64 <oliveira.vinicius64@gmail.com>
2024-09-23 22:06:19 -0300

    test(AppTest): add test for lists of same size

--- src/test/java/ifsp/tc1/AppTest.java
+++ src/test/java/ifsp/tc1/AppTest.java
@@ -8,10 +8,22 @@ public class AppTest {
     @Test
     @DisplayName("All lists are empty")
     void allListsAreEmpty() {
+        App app = new App();
         int[] a = {1};
         int[] b = {};
         int[] expected = {};
-        int[] result = App.intercalate(a, b);
+        int[] result = app.intercalate(a, b);
         assertArrayEquals(expected, result);
     }
+    @Test
+    @DisplayName("Lists have size n")
+    void listsHaveSizeN() {
+        App app = new App();
+        app.setN(3);
+        int[] a = {1, 2, 3};
+        int[] b = {4, 5, 6};
+        boolean result = app.listsHaveNLength(a, b);
+        assertTrue(result);
+    }
+
 }
